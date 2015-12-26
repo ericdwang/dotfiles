77c4322c583dfe438a90d91e0f1ea0bb895df1c4
Eric Wang <gnawrice@gmail.com>
2015-12-26 12:46:14 -0800

    Add Ag and ptpython dotfiles

---
+++ dotfiles/.ptpython/config.py
@@ -0,0 +1,27 @@
+__all__ = ('configure',)
+
+
+def configure(repl):
+    # Disable autocompletion while typing
+    repl.complete_while_typing = False
+
+    # Show autosuggestions
+    repl.enable_auto_suggest = True
+
+    # Use current input when scrolling through history
+    repl.enable_history_search = True
+
+    # Highlight matching parenthesis
+    repl.highlight_matching_parenthesis = True
+
+    # Use classic prompt
+    repl.prompt_style = 'classic'
+
+    # Show line numbers for multiline input
+    repl.show_line_numbers = True
+
+    # Enable vi mode
+    repl.vi_mode = True
+
+    # Colorscheme
+    repl.use_code_colorscheme('monokai')
